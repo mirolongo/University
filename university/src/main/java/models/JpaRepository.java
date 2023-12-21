1ef4e7ad8400a0bc664449b40e145a4aef005200
mirolongo <70956059+mirolongo@users.noreply.github.com>
2023-12-21 15:13:44 +0100

    Repository files

--- university/src/main/java/models/JpaRepository.java
+++
@@ -1,4 +0,0 @@
-package models;
-
-public interface JpaRepository<T, T1> {
-}
--- university/src/main/java/models/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
